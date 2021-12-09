3121d0bd0e8287c0f260a16a8f8f1e0fba5a186a
Oguzhan Yürük <yuruk@math.tu-berlin.de>
2021-12-10 00:06:02 +0100

    Level generation changed: If number of polytopes is 3 and predef. triplets exist for chosen hardness, then a triplet is chosen with prob. 0.1

--- js/getTuples.js
+++ js/getTuples.js
@@ -6,31 +6,36 @@
 // get a data tuple containing the names of the chosen polytopes and their corresponding nets
 function getTuple(round, difficulty){
 	var polyData = data[difficulty];
-
-	// get the data randomly from the collection
+	var tuples = polyData['tuples'];
+	// get the data randomly from the collection with probability 0.9 and from tuples with probability 0.1
 	if (polyData['collection'].length){
-			var collection = polyData['collection'];	
-			var numOfPolys = collection.length;
-			var tuple = [];
-      var tupleNum = [];
-			for (var i=0; i<numberOfPolytopes; i++){
-				var temp = randomNumber(numOfPolys);
-				if (tupleNum.indexOf(temp) > -1) {
-					i--;
-					continue;
-				}
+		if(polyData['tuples'].length){
+			var rndnum = randomNumber(10);
+			if(rndnum<1 && numberOfPolytopes==3){ 
+				tuple = tuples[randomNumber(tuples.length)];
+			}else{
+				var collection = polyData['collection'];	
+				var numOfPolys = collection.length;
+				var tuple = [];
+     				var tupleNum = [];
+				for (var i=0; i<numberOfPolytopes; i++){
+					var temp = randomNumber(numOfPolys);
+					if (tupleNum.indexOf(temp) > -1) {
+						i--;
+						continue;
+					}
 				tupleNum.push(temp);
 				tuple.push(collection[temp]);
-			}
-		} 
-
-		// get the data from the predefined tuples
+				}
+			} 
+		}
+		// get the data from the predefined tuples if no data in collection
 		else{
-			var tuples = polyData['tuples'];
 			tuple = tuples[randomNumber(tuples.length)];
 		}
 	
 	return tuple
+	}
 }
 			
 
