c5270e2db99f3d26b277c28aa3840f0f56a39a09
Oguzhan Yürük <yuruk@math.tu-berlin.de>
2021-12-10 17:16:45 +0100

    Fixed a typo in rhombicubeoctahedron

--- data/desc/rhombicuboctahedron_easy.js
+++ data/desc/rhombicuboctahedron_easy.js
@@ -1,5 +1,5 @@
 descriptions.push(
-   lang = { "en": "A polytope with 24 vertices, 48 edges and 26 facets. This is an Archimedean solid and its name is rhombicuboctahedron.", "de": "Ein Polytop mit 24 Ecken, 48 Kanten und 26 Seiten. Dies ist ein archimedischer Körper und sein Name ist Rhombicuboktaeder."}
+   lang = { "en": "A polytope with 24 vertices, 48 edges and 26 facets. This is an Archimedean solid and its name is rhombicuboctahedron.", "de": "Ein Polytop mit 24 Ecken, 48 Kanten und 26 Seiten. Dies ist ein archimedischer Körper und sein Name ist Rhombikuboktaeder."}
 );
 
 prepareDescriptions();
