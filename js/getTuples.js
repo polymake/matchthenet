52b0acadeef6b5c08d5940c871c427fab48f1759
Oguzhan Yürük <yuruk@math.tu-berlin.de>
2021-12-10 12:51:31 +0100

    Fixed the bug for tuple generation for real this time.

--- js/getTuples.js
+++ js/getTuples.js
@@ -9,29 +9,25 @@ function getTuple(round, difficulty){
 	var tuples = polyData['tuples'];
 	// get the data randomly from the collection with probability 0.9 and from tuples with probability 0.1
 	if (polyData['collection'].length){
-		if(polyData['tuples'].length){
-			var rndnum = randomNumber(10);
-			if(rndnum<1 && numberOfPolytopes==3){ 
-				tuple = tuples[randomNumber(tuples.length)];
-			}else{
-				var collection = polyData['collection'];	
-				var numOfPolys = collection.length;
-				var tuple = [];
-     				var tupleNum = [];
-				for (var i=0; i<numberOfPolytopes; i++){
-					var temp = randomNumber(numOfPolys);
-					if (tupleNum.indexOf(temp) > -1) {
-						i--;
-						continue;
-					}
-				tupleNum.push(temp);
-				tuple.push(collection[temp]);
+		var rndnum = randomNumber(10);
+		if(polyData['tuples'].length && rndnum<1 && numberOfPolytopes==3){
+			tuple = tuples[randomNumber(tuples.length)];
+		}else{
+			var collection = polyData['collection'];	
+			var numOfPolys = collection.length;
+			var tuple = [];
+     			var tupleNum = [];
+			for (var i=0; i<numberOfPolytopes; i++){
+				var temp = randomNumber(numOfPolys);
+				if (tupleNum.indexOf(temp) > -1) {
+					i--;
+					continue;
 				}
-			} 
-		}
-	}
-		// get the data from the predefined tuples if no data in collection
-	else{
+			tupleNum.push(temp);
+			tuple.push(collection[temp]);
+			}
+		} 
+	}else{// get the data from the predefined tuples if no data in collection
 		tuple = tuples[randomNumber(tuples.length)];
 	}	
 	return tuple	
